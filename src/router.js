dd7d48abb9692fcb420b0539c43d30fce73c477a
unknown <paulo.prsdesouza@gmail.com>
2019-05-19 12:24:32 -0300

    build

--- src/router.js
+++ src/router.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
-import About from './views/About.vue'
+import Site from './views/Site.vue'
 
 Vue.use(Router)
 
@@ -11,13 +10,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: Home
-    },
-    {
-      path: '/about',
-      name: 'about',
-      component: About
+      name: 'site',
+      component: Site
     }
   ]
 })
